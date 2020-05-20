304bc49fd17ce288e791d1105f6ca85b1bf20f0f
Max Han <mwellh84@gmail.com>
2020-05-19 20:03:12 -0400

    update start script for heroku

--- server/db/db.js
+++ server/db/db.js
@@ -4,9 +4,14 @@ mongoose.set('debug', true)
 // Database Name
 const dbName = 'boilermaker-mongo';
 
-// Connection URL
+// Connection URL local
 const url = `mongodb://localhost/${dbName}`
+
+//Connection URL Mongo Atlas
+// const url = "mongodb+srv://maxwell:reactapp@mongotomeeting-f3ssf.mongodb.net/test?retryWrites=true&w=majority";
+
 const db = mongoose.connect(url)
+console.log('#@########db connected at ', url)
 mongoose.Promise = Promise
 
 module.exports = db
