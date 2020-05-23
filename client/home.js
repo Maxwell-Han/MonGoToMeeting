a8c29f00e93b25b39a07600e820096d61cefc46a
Max Han <mwellh84@gmail.com>
2020-05-23 10:38:32 -0400

    add bottom split pane

--- client/home.js
+++ client/home.js
@@ -3,25 +3,29 @@ import { connect } from "react-redux";
 import "./index.css";
 import { logout } from "./store";
 import * as Space from "react-spaces";
-import { ViewPort, Fill, BottomResizable } from "react-spaces";
+import { ViewPort, Fill, BottomResizable, LeftResizable } from "react-spaces";
 
 const Home = (props) => {
   const { handleLogout } = props;
   return (
-    <div>
-      <ViewPort>
+    <ViewPort>
+      <Fill>
+        <h4>Welcome to the home page</h4>
+        <h3>You have been logged in!</h3>
+        <h3>{`Welcome ${props.user.email}`}</h3>
+      </Fill>
+      <BottomResizable size={100} style={{ border: "2px solid blue" }}>
+        <LeftResizable size={60} style={{ border: "2px solid blue" }}>
+          <h4>Left chat menu </h4>
+          <h3>Goes Here</h3>
+        </LeftResizable>
         <Fill>
-          <h4>Welcome to the home page</h4>
-          <h3>You have been logged in!</h3>
-          <h3>{`Welcome ${props.user.email}`}</h3>
-        </Fill>
-        <BottomResizable size={100} style={{border:'2px solid blue'}}>
           <a href="#" onClick={handleLogout}>
             Logout
           </a>
-        </BottomResizable>
-      </ViewPort>
-    </div>
+        </Fill>
+      </BottomResizable>
+    </ViewPort>
   );
 };
 
