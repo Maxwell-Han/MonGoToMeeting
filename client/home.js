06a74d04862847f220688c82e593af8420e97724
Max Han <mwellh84@gmail.com>
2020-05-23 10:33:27 -0400

    add resizable components

--- client/home.js
+++ client/home.js
@@ -2,33 +2,41 @@ import React, { useEffect, useContext } from "react";
 import { connect } from "react-redux";
 import "./index.css";
 import { logout } from "./store";
+import * as Space from "react-spaces";
+import { ViewPort, Fill, BottomResizable } from "react-spaces";
 
 const Home = (props) => {
   const { handleLogout } = props;
   return (
     <div>
-      <h4>Welcome to the home page</h4>
-      <h3>You have been logged in!</h3>
-      <h3>{`Welcome ${props.user.email}`}</h3>
-      <a href="#" onClick={handleLogout}>
-        Logout
-      </a>
+      <ViewPort>
+        <Fill>
+          <h4>Welcome to the home page</h4>
+          <h3>You have been logged in!</h3>
+          <h3>{`Welcome ${props.user.email}`}</h3>
+        </Fill>
+        <BottomResizable size={100} style={{border:'2px solid blue'}}>
+          <a href="#" onClick={handleLogout}>
+            Logout
+          </a>
+        </BottomResizable>
+      </ViewPort>
     </div>
   );
 };
 
-const mapState = state => {
+const mapState = (state) => {
   return {
     isLoggedIn: !!state.user.id,
-    user: state.user
+    user: state.user,
   };
 };
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleLogout() {
       dispatch(logout());
-    }
+    },
   };
 };
 
