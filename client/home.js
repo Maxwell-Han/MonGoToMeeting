544c5fa18c2d9f2a292d34e2e3151d070c0a4aa0
Max Han <mwellh84@gmail.com>
2020-05-24 09:47:24 -0400

    Add main page layout

--- client/home.js
+++ client/home.js
@@ -4,25 +4,55 @@ import "./index.css";
 import { logout } from "./store";
 import * as Space from "react-spaces";
 import { ViewPort, Fill, BottomResizable, LeftResizable } from "react-spaces";
+import { Header, Nav, Box, TextArea } from "grommet";
 
 const Home = (props) => {
   const { handleLogout } = props;
   return (
     <ViewPort>
       <Fill>
+        <Header background="brand">
+          <h3>Menu goes here</h3>
+          <Nav direction="row">
+            <a href="#" onClick={handleLogout}>
+              Logout
+            </a>
+          </Nav>
+        </Header>
         <h4>Welcome to the home page</h4>
         <h3>You have been logged in!</h3>
         <h3>{`Welcome ${props.user.email}`}</h3>
       </Fill>
-      <BottomResizable size={100} style={{ border: "2px solid blue" }}>
-        <LeftResizable size={60} style={{ border: "2px solid blue" }}>
-          <h4>Left chat menu </h4>
-          <h3>Goes Here</h3>
+      <BottomResizable size={500} style={{ border: "2px solid blue" }}>
+        <LeftResizable size={360} style={{ border: "2px solid blue" }}>
+          <Fill>
+            <h4>Rooms Menu</h4>
+          </Fill>
+          <BottomResizable size={200} style={{ border: "2px solid aliceblue" }}>
+            <h3>Contacts</h3>
+          </BottomResizable>
         </LeftResizable>
         <Fill>
-          <a href="#" onClick={handleLogout}>
-            Logout
-          </a>
+          <Box border={{ color: "brand", size: "large" }} fill>
+            <Header background="brand">
+              <h3>Chat Menu goes here</h3>
+            </Header>
+            <Box basis="3/4">
+              <h4>Chat messages go here</h4>
+              <a href="#" onClick={handleLogout}>
+                Logout
+              </a>
+            </Box>
+            <Box basis="auto" align="end">
+              <Box fill>
+                <TextArea
+                  value={
+                    "Hello world Hello world Hello worldHello world Hello world"
+                  }
+                ></TextArea>
+              </Box>
+            </Box>
+          </Box>
         </Fill>
       </BottomResizable>
     </ViewPort>
